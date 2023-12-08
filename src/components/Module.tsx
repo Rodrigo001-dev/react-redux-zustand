b88d6b68444e08fff7e3f8ce6a2e7567a5c00370
Rodrigo Rael <rodrigorael53@gmail.com>
2023-12-08 13:29:09 -0300

    :sparkles: feat: add Collapsible from radix

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,3 +1,4 @@
+import * as Collapsible from '@radix-ui/react-collapsible'
 import { ChevronDown } from "lucide-react";
 
 import { Lesson } from "./Lesson";
@@ -10,8 +11,8 @@ interface ModuleProps {
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
   return (
-    <div>
-      <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+    <Collapsible.Root className='group'>
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
         <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
           {moduleIndex + 1}
         </div>
@@ -21,15 +22,17 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
           <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
         </div>
 
-        <ChevronDown className="w-5 h-5 ml-auto text-zinc-400" />
-      </button>
+        <ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform duration-300" />
+      </Collapsible.Trigger>
 
-      <nav className="relative flex flex-col gap-4 p-6">
-        <Lesson title="Fundamentos do Redux" duration="09:14" />
-        <Lesson title="Fundamentos do Redux" duration="09:14" />
-        <Lesson title="Fundamentos do Redux" duration="09:14" />
-        <Lesson title="Fundamentos do Redux" duration="09:14" />
-      </nav>
-    </div>
+      <Collapsible.Content>
+        <nav className="relative flex flex-col gap-4 p-6">
+          <Lesson title="Fundamentos do Redux" duration="09:14" />
+          <Lesson title="Fundamentos do Redux" duration="09:14" />
+          <Lesson title="Fundamentos do Redux" duration="09:14" />
+          <Lesson title="Fundamentos do Redux" duration="09:14" />
+        </nav>
+      </Collapsible.Content>
+    </Collapsible.Root>
   )
 }
