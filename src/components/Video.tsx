52826d7e5e75b3a8ba822fd5a4b706be07acb26d
Rodrigo Rael <rodrigorael53@gmail.com>
2023-12-09 14:46:23 -0300

    :sparkles: feat: default autoplay

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,25 +1,35 @@
-import ReactPlayer from "react-player";
+import ReactPlayer from 'react-player'
+import { useDispatch } from "react-redux";
 
 import { useAppSelector } from "../store";
+import { next } from "../store/slices/player";
 
 export function Video() {
+  const dispatch = useDispatch()
+
   const lesson = useAppSelector(state => {
     const { currentModuleIndex, currentLessonIndex } = state.player
 
-    const currentLesson = 
+    const currentLesson =
       state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
-    
+
     return currentLesson
   })
-  
+
+  function handlePlayNext() {
+    dispatch(next())
+  }
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
+        playing
+        onEnded={handlePlayNext}
         url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
-  )
+  );
 }
